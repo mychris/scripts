021bea1798d4a5ade9912f275ee9067193e8b36a
Christoph Göttschkes <just.mychris@googlemail.com>
2013-01-08 14:51:32 +0100

    changed script completely

--- colors3.sh
+++ colors3.sh
@@ -1,48 +1,26 @@
 #!/bin/bash
 #
-# Description:
+#   This file echoes a bunch of color codes to the 
+#   terminal to demonstrate what's available.  Each 
+#   line is the color code of one forground color,
+#   out of 17 (default + 16 escapes), followed by a 
+#   test use of that color on all nine background 
+#   colors (default + 8 escapes).
 #
-#    Prints a color table of 8bg * 8fg * 2 states (regular/bold)
-#
-# Copyright:
-#
-#    (C) 2009 Wolfgang Frisch <xororand@unfoog.de>
-#
-# License:
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-echo
-echo Table for 16-color terminal escape sequences.
-echo Replace ESC with \\033 in bash.
-echo
-echo "Background | Foreground colors"
-echo "---------------------------------------------------------------------"
-for((bg=40;bg<=47;bg++)); do
-	for((bold=0;bold<=1;bold++)) do
-		echo -en "\033[0m"" ESC[${bg}m   | "
-		for((fg=30;fg<=37;fg++)); do
-			if [ $bold == "0" ]; then
-				echo -en "\033[${bg}m\033[${fg}m [${fg}m  "
-			else
-				echo -en "\033[${bg}m\033[1;${fg}m [1;${fg}m"
-			fi
-		done
-		echo -e "\033[0m"
-	done
-	echo "--------------------------------------------------------------------- "
+T='gYw'   # The test text
+
+echo -e "\n                 40m     41m     42m     43m\
+     44m     45m     46m     47m";
+
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+           '  36m' '1;36m' '  37m' '1;37m';
+  do FG=${FGs// /}
+  echo -en " $FGs \033[$FG  $T  "
+  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+    do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+  done
+  echo;
 done
-
-echo
 echo
