55c8c985e5f17eab09cb804c9e0cbe38f834c592
Christoph Göttschkes <just.mychris@googlemail.com>
2015-11-23 22:39:51 +0100

    Removed usage of echo with printf

--- colors3.sh
+++ colors3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-#   This file echoes a bunch of color codes to the 
+#   This file displays a bunch of color codes to the 
 #   terminal to demonstrate what's available.  Each 
 #   line is the color code of one forground color,
 #   out of 17 (default + 16 escapes), followed by a 
@@ -10,17 +10,18 @@
 
 T='gYw'   # The test text
 
-echo -e "\n                 40m     41m     42m     43m\
-     44m     45m     46m     47m";
+printf "\n                 40m     41m     42m     43m     44m     45m     46m     47m\n";
 
 for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
            '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
-           '  36m' '1;36m' '  37m' '1;37m';
-  do FG=${FGs// /}
-  echo -en " $FGs \033[$FG  $T  "
-  for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-    do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+           '  36m' '1;36m' '  37m' '1;37m'; do
+  FG=${FGs// /}
+  printf " %s \x1B[%s  %s  " "$FGs" "$FG" "$T"
+  for BG in 40m 41m 42m 43m 44m 45m 46m 47m; do
+      printf "%s \x1B[%s\x1B[%s  %s  \x1B[0m" "$EINS" "$FG" "$BG" "$T"
   done
-  echo;
+  printf "\n"
 done
-echo
+printf "\n"
+
+# vim: ft=sh ts=2 sts=2 sw=2 et:
