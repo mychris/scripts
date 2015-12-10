cbc2e2ea32716493af66d63701a9163e5469d76b
Christoph Göttschkes <just.mychris@googlemail.com>
2015-12-10 17:46:10 +0100

    Use string.format

--- colors1.py
+++ colors1.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
   for i in range(2):
     for j in range(30, 38):
       for k in range(40, 48):
-        sys.stdout.write("\33[%d;%d;%dm%d;%d;%d\33[m " % (i, j, k, i, j, k))
+        sys.stdout.write("\33[{0};{1};{2}m{0};{1};{2}\33[m ".format(i, j, k))
       sys.stdout.write(os.linesep)
 
 # vim: ft=python ts=2 sts=2 sw=2 et:
