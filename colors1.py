ef99bb92feb79293b3a7c02de72a58de1923057b
Christoph Göttschkes <just.mychris@googlemail.com>
2015-12-10 17:41:17 +0100

    Fixed indention.

--- colors1.py
+++ colors1.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# vim: set ft=python :
 # pylint: disable=C0103,W0622,W0702,W1401
 
 import os
 import sys
 
 if __name__ == "__main__":
-    for i in range(2):
-        for j in range(30, 38):
-            for k in range(40, 48):
-                sys.stdout.write("\33[%d;%d;%dm%d;%d;%d\33[m " %
-                        (i, j, k, i, j, k))
-            sys.stdout.write(os.linesep)
+  for i in range(2):
+    for j in range(30, 38):
+      for k in range(40, 48):
+        sys.stdout.write("\33[%d;%d;%dm%d;%d;%d\33[m " % (i, j, k, i, j, k))
+      sys.stdout.write(os.linesep)
 
+# vim: ft=python ts=2 sts=2 sw=2 et:
