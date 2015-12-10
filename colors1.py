2a966df9fa8446f89cda5c7d4c314625502cfc0a
Christoph Göttschkes <just.mychris@googlemail.com>
2015-12-10 17:34:30 +0100

    Removed future import.

--- colors1.py
+++ colors1.py
@@ -3,7 +3,8 @@
 # vim: set ft=python :
 # pylint: disable=C0103,W0622,W0702,W1401
 
-from __future__ import print_function
+import os
+import sys
 
 if __name__ == "__main__":
     try:
@@ -14,7 +15,7 @@ if __name__ == "__main__":
     for i in range(2):
         for j in range(30, 38):
             for k in range(40, 48):
-                print("\33[%d;%d;%dm%d;%d;%d\33[m " %
-                        (i, j, k, i, j, k), end="")
-            print()
+                sys.stdout.write("\33[%d;%d;%dm%d;%d;%d\33[m " %
+                        (i, j, k, i, j, k))
+            sys.stdout.write(os.linesep)
 
